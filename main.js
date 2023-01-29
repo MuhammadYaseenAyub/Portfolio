48e007f13638a6b1cbc7d4e72cde22deba1dde18
Muneeb Ahmed <65030135+muneebahmedayub@users.noreply.github.com>
2023-01-29 14:24:52 +0500

    Revert "Dev muneeb"

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 import "./style.css";
-import gsap from "gsap";
-import { Flip } from "gsap/Flip";
-
-gsap.registerPlugin(Flip);
 
 document.querySelectorAll(".left-section > .tabs > .tab").forEach((tab) => {
   tab.addEventListener("click", (e) => {
     const tabName = e.currentTarget.getAttribute("data-tab");
-    if (!tabName) return;
-
-    const hr = document.querySelector(".tab.active > hr");
-    const state = Flip.getState(hr);
-
-    document.querySelectorAll(".left-section > .tabs > .tab").forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    const currentTarget = e.currentTarget;
-
-    currentTarget.classList.add("active");
-    currentTarget.appendChild(hr);
-    Flip.from(state, { duration: 1.25, absolute: true, ease: "elastic.out(1, 0.5)" });
+    console.log({tabName, e})
+    if(!tabName) return;
+    document
+      .querySelectorAll(".left-section > .tabs > .tab")
+      .forEach((tab) => {
+        tab.classList.remove("active");
+      });
+    e.currentTarget.classList.add("active");
     document
       .querySelectorAll(".left-section > .tab-content")
       .forEach((content) => {
         content.classList.remove("active");
       });
+      console.log(`[data-tab=${tabName}]`)
     document.getElementById(tabName).classList.add("active");
   });
 });
